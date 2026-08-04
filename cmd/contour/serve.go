c2e5754b509dd09587c84f45bd86380646dd73d9
Max Denushev <mdenushev@ya.ru>
2026-08-04 11:25:24 +0300

    fix(controller): exclude helm and service account secrets

--- cmd/contour/serve.go
+++ cmd/contour/serve.go
@@ -235,6 +235,9 @@ func NewServer(log logrus.FieldLogger, ctx *serveContext) (*Server, error) {
 			// This is useful for saving memory by removing fields that are not needed by Contour.
 			ByObject: map[client.Object]ctrl_cache.ByObject{
 				&core_v1.Secret{}: {
+					Field: Field: fields.ParseSelectorOrDie(
+						"type!=helm.sh/release.v1,type!=kubernetes.io/service-account-token"
+					),
 					Transform: func(obj any) (any, error) {
 						secret, ok := obj.(*core_v1.Secret)
 						// TransformFunc should handle the tombstone of type cache.DeletedFinalStateUnknown
